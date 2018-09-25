8fc0f07fffc46d6e4195b3c586bf88be63f227df
Abdel Benamrouche <abdel@unxp.fr>
2018-09-25 11:09:37 +0200

    start qemu directly on console
    Signed-off-by: Abdel Benamrouche <abdel@unxp.fr>

--- scripts/start_qemu.sh
+++ scripts/start_qemu.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -hda ./hdd.img -kernel kernel/linux-4.4.157/arch/x86_64/boot/bzImage -boot c -append root=/dev/sda --enable-kvm
+qemu-system-x86_64 -hda ./hdd.img -kernel kernel/linux-4.4.157/arch/x86_64/boot/bzImage -boot c  -nographic -serial mon:stdio -append 'root=/dev/sda console=ttyS0' --enable-kvm
